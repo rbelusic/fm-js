2fd10716be5844872466b9264371be995ea9210b
Roberto Belusic <roberto.belusic@gmail.com>
2013-09-26 17:37:28 +0200

    Templates example

--- html/examples/004/ex004.js
+++ html/examples/004/ex004.js
@@ -1,10 +1,4 @@
-// == create static dm list ==========================================
-FM.DmList.addConfiguration('staticListExample', {
-    responseObject: FM.DmObject,
-    data: [ // for all query params
-        {id:1, name: "name1",description: "description 1"},
-        {id:2, name: "name2",description: "description 2"},
-        {id:3, name: "name3",description: "description 3"},
-        {id:4, name: "name4",description: "description 4"}
-    ]
-});
+// == create predefined template ==========================================
+FM.UtTemplate.addTemplate("predefined.dummy.html",
+    "<div>predefined dummy template</div>"
+);
